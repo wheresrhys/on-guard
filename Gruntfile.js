bec3e015f62f1ee2ff520205a9f70b606cfd1b80
Rhys Evans <wheresrhys@gmail.com>
2013-12-20 01:03:05 +0000

    tidied up Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -93,12 +93,7 @@ module.exports = function(grunt) {
                 }
             }
         },
-        uglify: {
-            dist: {
-                src: './lib/requirejs/require.js',
-                dest: './dist/lib/requirejs/require.js'
-            }
-        },
+
         browserify: {
             prod: {
                 options: {
@@ -118,6 +113,19 @@ module.exports = function(grunt) {
             },
             src: {
                 files: {
+                    'dist/bundle.src.js': ['./src/**/*.js', '!./src/main.js']
+                },
+                options: {
+                    debug: false,
+                    aliasMappings: {
+                        cwd: './src/',      // Src matches are relative to this path.
+                        src: ['**/*.js'], // Actual pattern(s) to match.
+                        dest: './theapp/'
+                    }
+                }
+            },
+            instrumented: {
+                files: {
                     'tmp/bundle.src.js': ['./dist/instrument/src/**/*.js', '!./dist/instrument/src/main.js']
                 },
                 options: {
@@ -125,9 +133,7 @@ module.exports = function(grunt) {
                     aliasMappings: {
                         cwd: './dist/instrument/src/',      // Src matches are relative to this path.
                         src: ['**/*.js'], // Actual pattern(s) to match.
-                        dest: './theapp/',   // Destination path prefix.
-                        //ext: '.js'   // Dest filepaths will have this extension.
-                        // src: ['./dist/instrument/src/**/*.js:theapp/**/*.js']
+                        dest: './theapp/'
                     }
                 }
             },
@@ -147,49 +153,24 @@ module.exports = function(grunt) {
                 lazy: false,
                 basePath: 'dist/instrument/'
             }
-        },
-        reloadTasks : {
-            rootPath : 'tmp/'
-        },
-        storeCoverage : {
-            options : {
-                dir : 'reports/'
-            }
-        },
-        makeReport : {
-            src : './reports/coverage.json',
-            options : {
-                type : 'html',
-                dir : './reports',
-                print : 'detail'
-            }
         }
     });
 
 
     // These plugins provide necessary tasks.
     grunt.loadNpmTasks('grunt-contrib-clean');
-    grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-jasmine');
     grunt.loadNpmTasks('grunt-contrib-jshint');
-    grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-contrib-requirejs');
     grunt.loadNpmTasks('grunt-contrib-htmlmin');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-sass');
-    grunt.loadNpmTasks('grunt-contrib-connect');
     grunt.loadNpmTasks('grunt-browserify');
     grunt.loadNpmTasks('grunt-istanbul');
-    grunt.loadNpmTasks('grunt-debug-task');
 
-    grunt.registerTask('cover', [ 'instrument', 'browserify:src', 'browserify:test', 'jshint:lenient', 'coverup']);
-
-    grunt.registerTask('coverup', ['jasmine:run', 'storeCoverage', 'makeReport' ]);
-    
     // Default task.
-    grunt.registerTask('test', ['browserify:src', 'browserify:test', 'jshint:lenient', 'jasmine:run', 'cleanRunner']);
+    grunt.registerTask('test', ['instrument', 'browserify:instrumented', 'browserify:src', 'browserify:test', 'jshint:lenient', 'jasmine:run', 'cleanRunner']);
     grunt.registerTask('lint', ['jshint:strict']);
-    grunt.registerTask('build', ['test', 'clean', 'sass:dist', 'requirejs:dist', 'htmlmin:dist', 'uglify:dist', 'miscBuildTasks']);
+    grunt.registerTask('build', ['test', 'clean', 'sass:dist', 'htmlmin:dist', 'miscBuildTasks']);
 
     grunt.registerTask('cleanRunner', function () {
         var fs = require('fs'),
@@ -198,7 +179,9 @@ module.exports = function(grunt) {
             if (err) {
                 throw err;
             }
-            fs.writeFile('specRunner.html', data.replace('src=".grunt/grunt-contrib-jasmine/dist', 'src="./dist')
+            fs.writeFile('specRunner.html', data
+                .replace('src=".grunt/grunt-contrib-jasmine/dist', 'src="./dist')
+                .replace('src="./tmp', 'src="./dist')
                 .replace('\'.grunt/grunt-contrib-jasmine/grunt-template-jasmine-istanbul/reporter.js\',\'./.grunt/grunt-contrib-jasmine/reporter.js\'', ''), function (err) {
                 if (err) {
                     throw err;
